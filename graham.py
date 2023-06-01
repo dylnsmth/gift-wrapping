c5273506a80a947f104f46e5c2335e6ee7731381
MrMath142 <aydn.math@gmail.com>
2023-06-01 12:05:56 -0500

    fixed it!

--- graham.py
+++ graham.py
@@ -15,7 +15,10 @@ def get_polar_points(points):
 	polar_points = []
 	for point in points:
 		r = math.sqrt(point[0]**2 + point[1]**2)
-		theta = math.atan(1.0 * point[1] / point[0])
+		if point[0] == 0:
+			theta = 0
+		else:
+			theta = math.atan(1.0 * point[1] / point[0])
 		new_point = [r, theta]
 		polar_points.append(new_point)
 	return polar_points
@@ -26,20 +29,41 @@ def extend_into_polar(points):
 		points[i] = point + polar_points[i]
 	return points
 
+def constrict_from_polar(points):
+	new_points = []
+	for i, point in enumerate(points):
+		new_points.append([point[0], point[1]])
+	return new_points
+
 def recenter_points(points, center):
+	for point in points:
+		point[0] -= center[0]
+		point[1] -= center[1]
+	return points
+
+def uncenter_points(points, center):
 	for point in points:
 		point[0] += center[0]
 		point[1] += center[1]
 	return points
 
-def angle_betwixt(point1, point2, point3):
-	point1[0] -= point2[0]
-	point1[1] -= point2[1]
-	point3[0] -= point2[0]
-	point3[1] -= point2[1]
-	# using dot product formula
-	angle = math.acos((point1[0]*point3[0] + point1[1]*point3[1]) / (math.hypot(point1[0], point1[1]) * math.hypot(point3[0], point3[1])))
-	print(angle)
+def orientation(point1, point2, point3):
+	value = (point2[1] - point1[1])*(point3[0]-point2[0]) - (point2[0]-point1[0])*(point3[1]- point2[1])
+	return value
+		
+
+# def angle_betwixt(point1, point2, point3):
+#	print(point1)
+#	print(point2)
+#	print(point3)
+#	point1[0] -= point2[0]
+#	point1[1] -= point2[1]
+#	point3[0] -= point2[0]
+#	point3[1] -= point2[1]
+#	# using dot product formula
+#	angle = math.acos((point1[0]*point3[0] + point1[1]*point3[1]) / (math.hypot(point1[0], point1[1]) * math.hypot(point3[0], point3[1])))
+#	print(angle)
+
 
 def graham_scan(points):
 	min_point = get_minimum_y(points)
@@ -47,10 +71,16 @@ def graham_scan(points):
 	points = recenter_points(points, min_point)
 	points = extend_into_polar(points)
 	points = sorted(points, key=lambda element: (element[3], element[2]))
+	points = constrict_from_polar(points)
+	points = uncenter_points(points, min_point)
+
 	stack = [min_point, points[0], points[1]]
 	for i in range(2, len(points)):
-		pass
-	angle_betwixt(min_point, points[0], points[1])
-		
-	
-graham_scan(points)
+		stack_end = len(stack)-1
+		while orientation(stack[stack_end-1], stack[stack_end], points[i]) >= 0:
+			stack.remove(stack[stack_end])
+			stack_end = len(stack)-1
+		stack.append(points[i])
+
+
+	return stack
