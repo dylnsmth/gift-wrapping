1c6c2fd3f096e6cf529205a6e7c368ea4c34718e
kyra-h <helmboldk@carleton.edu>
2023-06-01 14:08:13 -0500

    EEEEE

--- Jarvis.py
+++ Jarvis.py
@@ -5,30 +5,45 @@ class Jarvis:
         hull = []
         # get the leftmost point , initialize to cur
         left = points[0]
+        right = points[0]
         for p in points:
             if p[0] < left[0]:
-                left[0] = p[0]
+                left = p
+            if p[0] > right[0]:
+                right = p
+
         curPoint = left 
-        curSlope = 1000000
+        curSlope = math.inf
         hull.append(left)
         hullFinished = False
+        rightReached = False
 
         # while we haven't reached the original point again:
         while hullFinished == False:
-            # choose the point that has the greatest angle w/ respect to the current point
-            if len(points) <= 1:
+            if len(points) == 0:
                 break
-            bestPoint = points[1]
-            minSlopeDifference = 100000
+            bestPoint = points[0]
+            minSlopeDifference = math.inf
+            
+            # choose the point that has the greatest angle w/ respect to the current point
             for p in points:
                 if curPoint[0] != p[0]:
                     pSlope = (curPoint[1] - p[1]) / (curPoint[0] - p[0])
-                    if abs(curSlope - pSlope) < minSlopeDifference:
-                        minSlopeDifference = abs(curSlope - pSlope)
+                    dif = abs(curSlope - pSlope) # calculate difference in slopes
+                    print(dif)
+                    # until we reach the rightmost point, we want the slope to be the closest one that is smaller than the current slope 
+                    if (dif < minSlopeDifference or minSlopeDifference == math.inf) and not p == left and not rightReached and dif != 0:
+                        minSlopeDifference = dif
                         bestPoint = p
-                
+                    elif (dif < minSlopeDifference or minSlopeDifference == math.inf) and rightReached:
+                        minSlopeDifference = dif
+                        bestPoint = p
+                            
             #remove bestPoint from points
             points.remove(bestPoint)
+
+            if bestPoint == right:
+                rightReached = True
 
             hull.append(bestPoint)
 
